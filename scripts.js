e9846385204fd8ea451b7a15d12963ce55204397
Aaryan Sharma <87175338+sharmaaaryan4012@users.noreply.github.com>
2024-06-13 15:30:53 +0530

    Add files via upload
    New website

---
+++ scripts.js
@@ -0,0 +1,11 @@
+/*
+    Project: Personal Portfolio
+    Owner: Aaryan Sharma
+    Date: 13th June 2024
+*/
+
+
+function toggleDropdown() {
+  const dropdown = document.getElementById('dropdownMenu');
+  dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
