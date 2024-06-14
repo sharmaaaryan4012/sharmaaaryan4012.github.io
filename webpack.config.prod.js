8a50f18fd02c46cbed9a08031d125ff170c0cc9c
Aaryan Sharma <87175338+sharmaaaryan4012@users.noreply.github.com>
2024-06-15 03:27:31 +0530

    Updated - added BG

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './home.html',
     }),
     new CopyPlugin({
       patterns: [
